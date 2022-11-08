295f8c4a93b407db98649139de95b9dfe9b9fd65
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-08 13:28:55 +0700

    fix migration and model

--- database/migrations/2022_11_08_061918_create_questions_table.php
+++ database/migrations/2022_11_08_061918_create_questions_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->longText('question');
+            $table->longText('answer')->nullable();
+            $table->boolean('is_read')->default(false);
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questions');
     }
 };
